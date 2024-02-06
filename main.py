42d5c6aa4e9786657d8cc06a02a90dcd617d34c0
Kirill <elistratovkirill3@gmail.com>
2024-02-06 17:30:18 +0500

    2 commit

--- main.py
+++ main.py
@@ -38,4 +38,46 @@ class MapParams(object):
 
 
 def load_map(mp):
-    pass
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+
+    if not response.ok:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+        sys.exit(2)
+
+    return map_file
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    mp = MapParams()
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEDOWN:
+                mp.decrease_zoom()  # уменьшение масштаба
+            elif event.key == pygame.K_PAGEUP:
+                mp.increase_zoom()  # увеличение масштаба
+        map_file = load_map(mp)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
+
+
+if __name__ == "main":
+    main()
